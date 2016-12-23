ddbdc08b1ca8b556f0a2242a1f1726f935cc2591
Anna Devlin <anna.devlin@ocsbstudent.ca>
2016-12-23 12:43:53 -0500

    Buttons made to react and splash screen updated

--- splash_scene.py
+++ splash_scene.py
@@ -17,18 +17,69 @@ class SplashScene(Scene):
         
         # create timer, so that after 2 seconds move to next scene
         self.start_time = time.time()
+        self.size_of_screen_x = self.size.x
+        self.size_of_screen_y = self.size.y
+        self.screen_center_x = self.size_of_screen_x/2
+        self.screen_center_y = self.size_of_screen_y/2
         
         # add MT blue background color
         self.background = SpriteNode(position = self.size / 2, 
                                      color = (0.61, 0.78, 0.87), 
                                      parent = self, 
                                      size = self.size)
-        self.school_crest = SpriteNode('./assets/sprites/MT_Crest.jpg',
-                                       parent = self,
-                                       position = self.size/2)
+        #self.school_crest = SpriteNode('./assets/sprites/MT_Crest.jpg',
+        #                               parent = self,
+        #                              position = Vector2(self.screen_center_x, self.screen_center_y))
+                                       
+        time.sleep(1)
+        #see about this after
+        self.logo = LabelNode(text = '{devlin}',
+                                      font = ('Didot', 45),
+                                      color = 'white',
+                                      parent = self,
+                                      position = Vector2(self.screen_center_x, self.screen_center_y))
+                                      
+        #logo_word = '{devlin}'
+        #for index in range (0, len(logo_word)):
+        #    self.logo(text.append(str(logo_word[index])),
+        #    time.sleep(.1)
+                                      
+        #self.logo.word = '{'
+        #time.sleep(.2)
+        #self.logo.word = '{d'
+        #time.sleep(.2)
+        
+        #for letter in '{devlin}':
+        #    typed_word += '{devlin}'(letter)
+        #    self.logo.text = typed_word
+        #    time.sleep(.2)
+            
+            #self.logo.text = typed_word 
+            #                          
+     #                                 for letter in str:
+    #sys.stdout.write(letter)
+    #time.sleep(.1)
+                                      
+        #self.logo_text = LabelNode(text = 'Design by: Anna Devlin',
+        #                              font = ('Times New Roman', 20),
+        #                              parent = self,
+        #                              position = self.size / 2)
+        #                              
     
     def update(self):
         # this method is called, hopefully, 60 times a second
+        
+        # have text wait to appear, then grow to appear
+        #if not self.presented_scene and time.time() - self.start_time #> 0.3:
+        #    self.text = LabelNode(text = '{devlin}',
+        #                              font = ('Times New Roman', 1),
+        #                              color = 'white',
+        #                              parent = self,
+        #                              position = Vector2(self.screen_center_x, self.screen_center_y - 300))
+            # to increase the size of the text gradually
+            # for index in range (0,20):
+            #    self.text.font = (('Times New Roman', int(index())))
+            
         
         # after 2 seconds, move to main menu scene
         if not self.presented_scene and time.time() - self.start_time > 2:
