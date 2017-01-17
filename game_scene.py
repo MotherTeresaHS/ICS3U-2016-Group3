0c2d2316cc93e5622dd5baec14fe709ce8a2ed70
Anna Devlin <anna.devlin@ocsbstudent.ca>
2017-01-17 09:44:02 -0500

    Button issues resolved; fish move across screen Next step is to make sure fish go away after they leave screen and when they intersect with character

--- game_scene.py
+++ game_scene.py
@@ -25,11 +25,11 @@ class GameScene(Scene):
         self.scale_size = 0.75
         self.score = 0
         self.alien_attack_rate = 1
-        self.scale_size = 0.80
+        self.scale_size = 0.25
         self.stroke_began = False
         self.fish = []
         self.fish_swim_speed = 20
-        self.new_fish_rate = 1
+        self.new_fish_rate = 4
         self.fishhook_enter_rate = 1
         self.game_over = False
         #self.score = 0
@@ -47,7 +47,7 @@ class GameScene(Scene):
         self.character = SpriteNode('./assets/sprites/character.png',
                                      parent = self,
                                      position = self.size/2,
-                                     size = self.size/2.7)
+                                     size = self.size/6)
                                      
     def update(self):
         # this method is called 60 times a second to update the game
@@ -60,9 +60,20 @@ class GameScene(Scene):
         
         # check every update if a fish is off screen
         for fish in self.fish:
-            if fish.position.x > self.size_of_screen_x - 10:
+            if fish.position.x > self.size_of_screen_x + 25:
                 fish.remove_from_parent()
                 self.fish.remove(fish)
+                print("fish removed")
+                print(len(self.fish))
+                
+        #check every update to see if the character has eaten a fish
+        #if len(self.fish) > 0:
+        for fish_eaten in self.fish:
+            if self.fish.frame.intersects(self.character.frame):
+                print("Fish was EATEN!")
+                self.score += 100
+                fish.remove_from_parent()
+                self.fish.remove(fish_eaten)
         
         #check every update if a fish is off screen
         #for fish in self.fish:
@@ -120,34 +131,71 @@ class GameScene(Scene):
         # back into use. Reload anything you might need.
         pass
         
+#    def add_fish(self):
+#        #add new fish to enter into screen
+#        #based on alien scripting by Mr Coxall
+#        
+#        print("fish added")
+#        
+#        fish_start_position = Vector2(2, 200)
+#        #fish_start_position = Vector2(-100,random.randint(100, int(self.size_of_screen_y - 100)))
+#        
+#        fish_end_position = Vector2(600, 15)
+#        
+#        #fish_end_position = Vector2(self.size_of_screen_x + 100,(random.randint(100, self.size_of_screen_y))
+        
+        
+        #THESE NEXT FOUR LINES OF CODE ARE THE ONES GETTING ERRORS, SPECIFICALLY THE LINE FOR THE POSITION
+#        self.fish.append(SpriteNode('./assets/sprites/fish.png',)
+#                                    position = Vector2(3,4),
+#                                    parent = self)
+                                    #fish_start_position,
+
+                                      
+        # make fish move across the screen
+#        fish_move_action = Action.move_to(fish_end_position.x, 
+#                                         fish_end_position.y, 
+#                                         self.fish_swim_speed)
+                                         #TIMING_SINODIAL)
+                                        
+                                         
+#        print(len(self.fish))
+#        self.fish[int(len(self.fish)-1)].run_action(fish_move_action)
+        #self.aliens[len(self.aliens)-1].run_action(alien_move_action)
+        
     def add_fish(self):
         #add new fish to enter into screen
         #based on alien scripting by Mr Coxall
         
         print("fish added")
         
-        fish_start_position = Vector2(2, 200)
-        #fish_start_position = Vector2(-100,random.randint(100, int(self.size_of_screen_y - 100)))
+        fish_start_position = Vector2(-200, 0)
+        fish_start_position_y = random.randint(100, int(self.size_of_screen_y - 100))
+        fish_start_position.y = fish_start_position_y
         
-        fish_end_position = Vector2(600, 15)
+        fish_end_position = Vector2(0, 0)
+        fish_end_position_x = int(self.size_of_screen_x + 100)
+        fish_end_position_y = random.randint(100, int(self.size_of_screen_y - 100))
+        fish_end_position.x = fish_end_position_x
+        fish_end_position.y = fish_end_position_y
         
         #fish_end_position = Vector2(self.size_of_screen_x + 100,(random.randint(100, self.size_of_screen_y))
         
         
         #THESE NEXT FOUR LINES OF CODE ARE THE ONES GETTING ERRORS, SPECIFICALLY THE LINE FOR THE POSITION
-        self.fish.append(SpriteNode('./assets/sprites/fish.png',)
-                                    position = Vector2(3,4),
-                                    parent = self)
-                                    #fish_start_position,
+        # add a fish, just 
+        self.fish.append(SpriteNode('./assets/sprites/fish.png',
+                             position = fish_start_position,
+                             scale = self.scale_size,
+                             parent = self))
 
                                       
         # make fish move across the screen
         fish_move_action = Action.move_to(fish_end_position.x, 
                                          fish_end_position.y, 
-                                         self.fish_swim_speed)
-                                         #TIMING_SINODIAL)
+                                         self.fish_swim_speed,
+                                         TIMING_SINODIAL)
                                         
                                          
         print(len(self.fish))
         self.fish[int(len(self.fish)-1)].run_action(fish_move_action)
-        #self.aliens[len(self.aliens)-1].run_action(alien_move_action)
