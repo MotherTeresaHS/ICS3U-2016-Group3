016fc013145a6daaf689c6d1a35d683c7e078836
annadevlin <anna.devlin@ocsbstudent.ca>
2017-01-20 12:51:51 -0500

    Update main_menu_scene.py

--- main_menu_scene.py
+++ main_menu_scene.py
@@ -2,6 +2,7 @@
 # Modified by: Anna Devlin
 # Added buttons, design, and connection to other scenes
 # Created on: Sep 2016
+# Modified on: Dec - Jan 2016
 # Created for: ICS3U
 # This scene shows the main menu.
 
@@ -27,7 +28,7 @@ class MainMenuScene(Scene):
         
         # add background color
         self.background = SpriteNode(position = self.size / 2, 
-                                     color = 'blue', 
+                                     color = '#2de4ff', 
                                      parent = self, 
                                      size = self.size)
                                      
@@ -47,6 +48,92 @@ class MainMenuScene(Scene):
                                        
         help_button_position = self.size / 2
         help_button_position.y = help_button_position.y - 190
+        self.help_button = SpriteNode('./assets/sprites/help_button.png',
+                                       parent = self,
+                                       position = help_button_position,
+                                       scale = self.scale_size)
+        
+        about_button_position = self.size / 2
+        about_button_position.y = about_button_position.y - 300
+        self.about_button = SpriteNode('./assets/sprites/about_button.png',
+                                       parent = self,
+                                       position = about_button_position,
+                                       scale = self.scale_size)
+    
+    def update(self):
+        # this method is called 60 times per second
+        pass
+    
+    def touch_began(self, touch):
+        # this method is called when user touches screen
+        
+        #to make the buttons react to being touched
+        if self.help_button.frame.contains_point(touch.location):
+            self.help_button.scale = self.shrink_size
+            sound.play_effect('./assets/sprites/Ding_3.caf') # this sound clip is from the Pythonista libraries
+            
+        if self.about_button.frame.contains_point(touch.location):
+            self.about_button.scale = self.shrink_size
+            sound.play_effect('./assets/sprites/Ding_3.caf') # this sound clip is from the Pythonista libraries
+            
+        if self.start_button.frame.contains_point(touch.location):
+            self.start_button.scale = self.shrink_size
+            sound.play_effect('./assets/sprites/Ding_3.caf') # this sound clip is from the Pythonista libraries
+    
+    def touch_moved(self, touch):
+        # this method is called, when user moves a finger around on the screen
+        
+        #to make the buttons revert to their original size when the user moves their finger
+        
+        if not self.start_button.frame.contains_point(touch.location):
+            self.start_button.scale = self.scale_size
+            
+        if not self.help_button.frame.contains_point(touch.location):
+            self.help_button.scale = self.scale_size
+            
+        if not self.about_button.frame.contains_point(touch.location):
+            self.about_button.scale = self.scale_size
+            
+        #to make the buttons shrink to their original size when the user moves their finger on top of them
+        if self.start_button.frame.contains_point(touch.location):
+            self.start_button.scale = self.shrink_size
+            
+        if self.help_button.frame.contains_point(touch.location):
+            self.help_button.scale = self.shrink_size
+            
+        if self.about_button.frame.contains_point(touch.location):
+            self.about_button.scale = self.shrink_size
+            
+    def touch_ended(self, touch):
+        # this method is called, when user releases a finger from the screen
+        
+        # if start button is pressed, go to game scene
+        if self.start_button.frame.contains_point(touch.location):
+            self.present_modal_scene(GameScene())
+            
+        # if start button is pressed, go to game scene
+        if self.help_button.frame.contains_point(touch.location):
+            self.present_modal_scene(HelpScene())
+            
+        if self.about_button.frame.contains_point(touch.location):
+            self.present_modal_scene(AboutScene())
+    
+    
+    def did_change_size(self):
+        # this method is called, when user changes the orientation of the screen
+        # thus changing the size of each dimension
+        pass
+    
+    def pause(self):
+        # this method is called, when user touches the home button
+        # save anything before app is put to background
+        pass
+    
+    def resume(self):
+        # this method is called, when user place app from background 
+        # back into use. Reload anything you might need.
+        pass
+    
         self.help_button = SpriteNode('./assets/sprites/help_button.png',
                                        parent = self,
                                        position = help_button_position,
