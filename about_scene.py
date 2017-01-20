f422682daa312152536b19961067698be88a88b9
annadevlin <anna.devlin@ocsbstudent.ca>
2017-01-20 12:45:07 -0500

    Update about_scene.py

--- about_scene.py
+++ about_scene.py
@@ -18,25 +18,24 @@ class AboutScene(Scene):
         self.size_of_screen_y = self.size.y
         self.screen_center_x = self.size_of_screen_x/2
         self.screen_center_y = self.size_of_screen_y/2
+        self.scale_size = 0.3
+        self.shrink_size = 0.25
         
         # add background color
         background_position = Vector2(self.screen_center_x, 
                               self.screen_center_y)
-        self.background = SpriteNode(position = background_position, 
+        self.background = SpriteNode('./assets/sprites/credits.png',
+                                     position = background_position, 
                                      color = 'green', 
                                      parent = self, 
                                      size = self.size)
-                                     
-        self.text = LabelNode(text = 'All contributors for artwork and code will be credited here',
-                                      font = ('Helvetica', 20),
-                                      parent = self,
-                                      position = Vector2(self.screen_center_x, self.screen_center_y))
                                       
         back_button_position = Vector2(100, 
                               self.size_of_screen_y - 100)
         self.back_button = SpriteNode('./assets/sprites/back_button.png',
                                        parent = self,
-                                       position = back_button_position)
+                                       position = back_button_position,
+                                       scale = self.scale_size)
         
     def update(self):
         # this method is called, hopefully, 60 times a second
@@ -44,9 +43,44 @@ class AboutScene(Scene):
     
     def touch_began(self, touch):
         # this method is called, when user touches the screen
-        pass
+        
+        #to make the back buttons react to the user touching them
+        if self.back_button.frame.contains_point(touch.location):
+            self.back_button.scale = self.shrink_size
     
     def touch_moved(self, touch):
+        # this method is called, when user moves a finger around on the screen
+        
+        #to make the back buttons react to the user moving onto them
+        if not self.back_button.frame.contains_point(touch.location):
+            self.back_button.scale = self.shrink_size
+           
+        #to make the back buttons react to the user moving off of them
+        if self.back_button.frame.contains_point(touch.location):
+            self.back_button.scale = self.scale_size
+    
+    def touch_ended(self, touch):
+        # this method is called, when user releases a finger from the screen
+        
+        # if start button is pressed, goto game scene
+        if self.back_button.frame.contains_point(touch.location):
+            self.dismiss_modal_scene()
+    
+    def did_change_size(self):
+        # this method is called, when user changes the orientation of the screen
+        # thus changing the size of each dimension
+        pass
+    
+    def pause(self):
+        # this method is called, when user touches the home button
+        # save anything before app is put to background
+        pass
+    
+    def resume(self):
+        # this method is called, when user place app from background 
+        # back into use. Reload anything you might need.
+        pass
+
         # this method is called, when user moves a finger around on the screen
         pass
     
