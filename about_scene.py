a158dc6ea8b8a6d546f184b5d62f8aa83c2c640c
annadevlin <anna.devlin@ocsbstudent.ca>
2017-01-23 14:23:12 -0500

    Updated about scene
    Most recent version

--- about_scene.py
+++ about_scene.py
@@ -80,29 +80,3 @@ class AboutScene(Scene):
         # this method is called, when user place app from background 
         # back into use. Reload anything you might need.
         pass
-
-        # this method is called, when user moves a finger around on the screen
-        pass
-    
-    def touch_ended(self, touch):
-        # this method is called, when user releases a finger from the screen
-        
-        # if start button is pressed, goto game scene
-        if self.back_button.frame.contains_point(touch.location):
-            self.dismiss_modal_scene()
-    
-    def did_change_size(self):
-        # this method is called, when user changes the orientation of the screen
-        # thus changing the size of each dimension
-        pass
-    
-    def pause(self):
-        # this method is called, when user touches the home button
-        # save anything before app is put to background
-        pass
-    
-    def resume(self):
-        # this method is called, when user place app from background 
-        # back into use. Reload anything you might need.
-        pass
-
