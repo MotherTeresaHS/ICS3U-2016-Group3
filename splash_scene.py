f78cac87724dbda58bafd67ace26027a900fdc03
annadevlin <anna.devlin@ocsbstudent.ca>
2017-01-23 14:19:43 -0500

    Updating splash scene
    Got rid of excess code and put in applicable comments

--- splash_scene.py
+++ splash_scene.py
@@ -27,62 +27,20 @@ class SplashScene(Scene):
                                      color = (0.61, 0.78, 0.87), 
                                      parent = self, 
                                      size = self.size)
-        #self.school_crest = SpriteNode('./assets/sprites/MT_Crest.jpg',
-        #                               parent = self,
-        #                              position = Vector2(self.screen_center_x, self.screen_center_y))
                                        
-        time.sleep(1)
-        #see about this after
+        #display logo
         self.logo = LabelNode(text = '{devlin}',
                                       font = ('Didot', 45),
                                       color = 'white',
                                       parent = self,
                                       position = Vector2(self.screen_center_x, self.screen_center_y))
                                       
-        #logo_word = '{devlin}'
-        #for index in range (0, len(logo_word)):
-        #    self.logo(text.append(str(logo_word[index])),
-        #    time.sleep(.1)
-                                      
-        #self.logo.word = '{'
-        #time.sleep(.2)
-        #self.logo.word = '{d'
-        #time.sleep(.2)
-        
-        #for letter in '{devlin}':
-        #    typed_word += '{devlin}'(letter)
-        #    self.logo.text = typed_word
-        #    time.sleep(.2)
-            
-            #self.logo.text = typed_word 
-            #                          
-     #                                 for letter in str:
-    #sys.stdout.write(letter)
-    #time.sleep(.1)
-                                      
-        #self.logo_text = LabelNode(text = 'Design by: Anna Devlin',
-        #                              font = ('Times New Roman', 20),
-        #                              parent = self,
-        #                              position = self.size / 2)
-        #                              
     
     def update(self):
         # this method is called, hopefully, 60 times a second
         
-        # have text wait to appear, then grow to appear
-        #if not self.presented_scene and time.time() - self.start_time #> 0.3:
-        #    self.text = LabelNode(text = '{devlin}',
-        #                              font = ('Times New Roman', 1),
-        #                              color = 'white',
-        #                              parent = self,
-        #                              position = Vector2(self.screen_center_x, self.screen_center_y - 300))
-            # to increase the size of the text gradually
-            # for index in range (0,20):
-            #    self.text.font = (('Times New Roman', int(index())))
-            
-        
         # after 2 seconds, move to main menu scene
-        if not self.presented_scene and time.time() - self.start_time > 2:
+        if not self.presented_scene and time.time() - self.start_time > 2.5:
             self.present_modal_scene(MainMenuScene())
     
     def touch_began(self, touch):
@@ -111,4 +69,4 @@ class SplashScene(Scene):
         # this method is called, when user place app from background 
         # back into use. Reload anything you might need.
         pass
-    
+  
